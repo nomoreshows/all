076f6fb8b3419e6a7b4eaf3bb15f1c7bd8794c72
elpiolito <jojo.jumper@hotmail.fr>
2014-07-16 21:18:10 +0200

    recaclcul moyenne after suppr

--- app/controllers/rates_controller.php
+++ app/controllers/rates_controller.php
@@ -299,12 +299,91 @@ class RatesController extends AppController {
 	}
 	
 	function admin_delete($id, $user_id) {
+		$myRate = $this->Rate->find('first',array('conditions' => array('Rate.id' =>$id)));
+		//var_dump($rate);
+		
+		//Recalcule du nb de notes total
+
+		// ajout -1 aux total des notes épisodes
+		$nbnotes = $this->Rate->find('count', array('conditions'=>array('Rate.episode_id' =>$myRate['Rate']['episode_id'])));
+		$this->Rate->Episode->id = $myRate['Rate']['episode_id'];
+		$this->Rate->Episode->saveField('nbnotes', $nbnotes-1);
+		
+		
+		// ajout +1 aux total des notes saisons
+		$episodes = $this->Rate->Season->Episode->find('all', array('conditions' => array('Episode.season_id' => $myRate['Rate']['season_id'])));
+		$totalnotes = 0;
+		foreach($episodes as $episode) {
+			$totalnotes += $episode['Episode']['nbnotes'];
+		}
+		$this->Rate->Season->id = $myRate['Rate']['season_id'];
+		$this->Rate->Season->saveField('nbnotes', $totalnotes);
+		
+		// ajout +1 aux total des notes series
+		$show = $this->Rate->Show->find('first', array('conditions' => array('Show.id' => $myRate['Rate']['show_id']), array('contain' => false), 'fields' => array('Show.id', 'Show.nbnotes')));
+		$totalnotes = $show['Show']['nbnotes'] - 1;
+
+		$this->Rate->Show->id = $myRate['Rate']['show_id'];
+		$this->Rate->Show->saveField('nbnotes', $totalnotes);
+		
 		//Suppression de la note de l'utilisateur
 		$this->Rate->del($id, true);
+		
+		// Recalcul de la moyenne de l'épisode 
+		$rates_episodes = $this->Rate->find('all', array('conditions'=>array('Rate.episode_id' =>$myRate['Rate']['episode_id'])));
+		if (!empty($rates_episodes)) {
+		  $total = 0;
+		  foreach($rates_episodes as $j => $rate) {
+			  $total += $rate['Rate']['name'];
+		  }
+		  $nb = $j+1;
+		  $moyenne = round($total / $nb, 2);
+		  $this->Rate->Episode->id = $myRate['Rate']['episode_id'];
+		  $this->Rate->Episode->saveField('moyenne', $moyenne);
+		}else{
+			$this->Rate->Episode->id = $myRate['Rate']['episode_id'];
+			$this->Rate->Episode->saveField('moyenne', 0);
+		}
+		
+		
+		// Recalcul de la moyenne de la saison
+		$episodessaison = $this->Rate->Episode->find('all', array('conditions' => array('Episode.season_id' => $myRate['Rate']['season_id']), 'fields' => array('Episode.moyenne')));
+		if (!empty($episodessaison)) {
+		  $total = 0;
+		  $nb = 0;
+		  foreach($episodessaison as $episode) {
+			  if ($episode['Episode']['moyenne'] != 0) {
+				$nb++;
+				$total += $episode['Episode']['moyenne'];
+			  }
+		  }
+		  $moyenne = round($total / $nb, 2);
+		  $this->Rate->Season->id = $myRate['Rate']['season_id'];
+		  $this->Rate->Season->saveField('moyenne', $moyenne);
+		}
+		
+		// Recalcul de la moyenne de la série
+		$saisonsserie = $this->Rate->Season->find('all', array('conditions' => array('Season.show_id' => $myRate['Rate']['show_id']), 'fields' => array('Season.moyenne')));
+		if (!empty($saisonsserie)) {
+		  $total = 0;
+		  $nb = 0;
+		  foreach($saisonsserie as $saison) {
+			  if ($saison['Season']['moyenne'] != 0) {
+				$nb++;
+				$total += $saison['Season']['moyenne'];
+			  }
+		  }
+		  $moyenne = round($total / $nb, 2);
+		  $this->Rate->Show->id = $myRate['Rate']['show_id'];
+		  $this->Rate->Show->saveField('moyenne', $moyenne);
+		}
+		
+		
 		
 		
 		$this->Session->setFlash('La note a été supprimée.'.$moyennes, 'growl');
         $this->redirect( array('controller' => 'rates', 'action' => 'searchresult', 'prefix' => 'admin', $user_id));
+		
 	}
 	
 	/*
